3e87740689f69031ed9c3ad181e2c6f3c8f0f9c5
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-08 23:12:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+// import React from 'react'
+// import '../styles/App.css';
+// import MealsProvider from './MealsProvider';
+// import MealsList from './MealsList';
+// import Counter from './Counter';
+
+// const App = () => {
+
+//   return (
+//     <div id="main">
+//       <MealsProvider>
+//         <MealsList />
+//         <Counter />
+//       </MealsProvider>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+
+import React from 'react';
 import '../styles/App.css';
 import MealsProvider from './MealsProvider';
 import MealsList from './MealsList';
 import Counter from './Counter';
 
 const App = () => {
-
   return (
     <div id="main">
       <MealsProvider>
@@ -13,8 +34,7 @@ const App = () => {
         <Counter />
       </MealsProvider>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
